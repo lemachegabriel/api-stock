dd224b41bc632d94252242d3ab84f338da667e0d
Gabriel Lemache <lemache35@gmail.com>
2021-06-08 09:42:11 -0300

    connect to atlas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
@@ -10,7 +12,7 @@ app.use(cors())
 
 //////////////////////////////////////////////
 
-mongoose.connect("mongodb://localhost/data", {useUnifiedTopology:true, useNewUrlParser:true}).then(() => {
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/data", {useUnifiedTopology:true, useNewUrlParser:true}).then(() => {
     console.log("sucsess");
 }).catch((err) => {
     console.log("error:"+err)
@@ -21,6 +23,6 @@ require('./src/model/routes')
 
 app.use("/api", require('./src/routes'));
 
-app.listen(8081, function(){
+app.listen(process.env.PORT || 8081, function(){
     console.log("server running http://localhost:8081");
 });
